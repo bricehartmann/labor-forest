202f57624e5335bcd65579360c69f6cbdd69a827
Brice Hartmann <brice@bricehartmann.com>
2026-08-05 01:06:03 -0500

    remove workspace bug fix

--- app/Filament/Pages/Project.php
+++ app/Filament/Pages/Project.php
@@ -597,7 +597,7 @@ class Project extends Page implements HasActions, HasSchemas, HasTable
                             );
                         }),
                     Action::make('remove')
-                        ->hidden(fn ($record) => $record['is_primary'] || $record['status'] === WorkspaceStatus::READY->value)
+                        ->hidden(fn ($record) => $record['is_primary'] || $record['status'] === WorkspaceStatus::READY->value || $record['status'] === WorkspaceStatus::CHANGING->value)
                         ->label('Remove')
                         ->icon(Heroicon::Trash)
                         ->modal()
